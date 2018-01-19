10e4da956715ddcdfcb1643c80b7f6234d1b680a
nhouston87 <nhouston87@gmail.com>
2018-01-19 00:30:35 -0500

    Successful testing on boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>React Starter</h1>
+        <p>This is a simple react boilerplate app</p>
+      </div>
+    );
+  }
+}
